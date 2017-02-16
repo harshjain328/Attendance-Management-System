fcd00df0324497708453413634a69b049c457d6e
harshjain328 <harsh.jain328@gmail.com>
2017-02-16 20:47:40 +0530

    Update Login.java

--- src/Login.java
+++ src/Login.java
@@ -1,12 +1,7 @@
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
- */
-
-/**
  *
- * @author Sahil Chanchad
- */
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.sql.*;
